44dd7a9cf198c265d029184526f7a9c28a3b93ef
Max Yuan <maxyuan6717@gmail.com>
2021-04-18 17:54:45 -0400

    display recordings

--- src/App.js
+++ src/App.js
@@ -2,33 +2,53 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Frames from "./Frames";
 import configData from "./config.json";
-import axios from 'axios';
 
 function App() {
+  const exercises = ["arm_circles", "jumping_jacks", "knee_drives"];
+  let twods = [];
+  exercises.forEach((exercise) => {
+    const twod = require(`./recordings_json/twod/${exercise}.json`);
+    twods.push(twod);
+  });
   const [src, setSrc] = useState();
   const [data, setData] = useState();
-  const [test, setTest] = useState();
+  const [frame_index, setFrameIndex] = useState(0);
+
   useEffect(() => {
     Frames(configData.prodIP, setData, setSrc).start();
-    // axios.get('ws://127.0.0.1:4444/twod')
-    //   .then(dat => {
-    //     console.log(dat);
-    //   });
-  }, []);
 
+    const interval = setInterval(() => {
+      setFrameIndex(frame_index + 1);
+    }, 300);
+    return () => clearInterval(interval);
+  }, [frame_index]);
+
+  // console.log(frame_index);
   // console.log(data);
   // console.log(data && data.people ? Object.keys(data.people) : null);
 
   return (
     <div className="App">
-      <img src={src} />
+      {exercises.map((exercise, index) => (
+        <div key={index}>
+          <div>{exercise} Recording</div>
+          <img
+            src={`data:image/pnjpegg;base64,${
+              twods[index][frame_index % twods[index].length].src
+            }`}
+            alt={exercise}
+          />
+        </div>
+      ))}
+      <div>Live Feed</div>
+      <img src={src} alt="live_feed" />
       {data && data.people
-        ? Object.keys(data.people).map((person) => {
+        ? Object.keys(data.people).map((person, index) => {
             // console.log(data.people[person]);
             const keypoints = data.people[person].keypoints;
             if (keypoints.RElbow && keypoints.LElbow)
               return (
-                <div>
+                <div key={index}>
                   <div>Person {person}</div>
                   <div>X-Coordinate: {keypoints.RElbow[0].toFixed(2)}</div>
                   <div>Y-Coordinate: {keypoints.RElbow[1].toFixed(2)}</div>
@@ -44,6 +64,7 @@ function App() {
                   </div>
                 </div>
               );
+            return null;
           })
         : null}
     </div>
---
+++ src/util/parse_recordings.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import os
+import json
+
+url = "../recordings/"
+exercise = "knee_drives"
+
+parsed_frames = []
+parsed_twod = []
+pkl_frames = os.listdir(url + exercise)
+
+for frame in pkl_frames:
+    frame_data = pd.read_pickle(url + exercise + "/" + frame)
+    parsed_frames.append(json.loads(frame_data["frame"]))
+    parsed_twod.append(json.loads(frame_data["twod"]))
+
+# print(parsed_frames[-1]["ts"] - parsed_frames[0]["ts"])
+
+with open("../recordings_json/frames/{}.json".format(exercise), "w") as f:
+    json.dump(parsed_frames, f, indent=3)
+
+with open("../recordings_json/twod/{}.json".format(exercise), "w") as f:
+    json.dump(parsed_twod, f, indent=3)
