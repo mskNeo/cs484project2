c0a3ea4108490ff8452fd009967d61fa51250779
Mahdeen (Neo) Khan <mahdeen.s.khan@yale.edu>
2021-04-18 19:26:25 -0400

    added python recording to site

--- src/App.js
+++ src/App.js
@@ -2,29 +2,26 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Frames from "./Frames";
 import configData from "./config.json";
-import axios from 'axios';
 
 function App() {
   const [src, setSrc] = useState();
   const [data, setData] = useState();
-  const [test, setTest] = useState();
+
   useEffect(() => {
     Frames(configData.devIP, setData, setSrc).start();
   }, []);
 
-  // console.log(data);
-  // console.log(data && data.people ? Object.keys(data.people) : null);
-
   return (
     <div className="App">
-      <img className="live" src={src} alt="live display" />
+      <div>Live Feed</div>
+      <img src={src} alt="live_feed" />
       {data && data.people
-        ? Object.keys(data.people).map((person) => {
+        ? Object.keys(data.people).map((person, index) => {
             // console.log(data.people[person]);
             const keypoints = data.people[person].keypoints;
             if (keypoints.RElbow && keypoints.LElbow)
               return (
-                <div>
+                <div key={index}>
                   <div>Person {person}</div>
                   <div>X-Coordinate: {keypoints.RElbow[0].toFixed(2)}</div>
                   <div>Y-Coordinate: {keypoints.RElbow[1].toFixed(2)}</div>
@@ -40,6 +37,7 @@ function App() {
                   </div>
                 </div>
               );
+            return null;
           })
         : null}
     </div>
