e7b4a8f8529278e9686e76a7a34da175ffc3d355
Mahdeen (Neo) Khan <mahdeen.s.khan@yale.edu>
2021-04-18 20:31:29 -0400

    have data output for live feed

--- src/App.js
+++ src/App.js
@@ -19,6 +19,30 @@ function App() {
     <div className="App">
       <div>Live Feed</div>
       <img className="live" src={live} alt="live feed" />
+      {liveData && liveData.people
+        ? Object.keys(liveData.people).map((person) => {
+            // console.log(liveData.people[person]);
+            const keypoints = liveData.people[person].keypoints;
+            if (keypoints.RElbow && keypoints.LElbow)
+              return (
+                <div>
+                  <div>Person {person}</div>
+                  <div>X-Coordinate: {keypoints.RElbow[0].toFixed(2)}</div>
+                  <div>Y-Coordinate: {keypoints.RElbow[1].toFixed(2)}</div>
+                  <div>Z-Coordinate: {keypoints.RElbow[2].toFixed(2)}</div>
+                  <div>
+                    {keypoints.RElbow[1] < 0 && keypoints.LElbow[1] < 0
+                      ? "Both Arms Raised!"
+                      : keypoints.RElbow[1] < 0
+                      ? "Right Arm Raised!"
+                      : keypoints.LElbow[1] < 0
+                      ? "Left Arm Raised!"
+                      : "Nothing"}
+                  </div>
+                </div>
+              );
+          })
+        : null}
       <img className="exercise" src={exercise} alt="exercise feed" />
     </div>
   );
