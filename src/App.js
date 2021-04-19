eeb4e3d627c92bb25f7460654423f10bc82ebaf2
Mahdeen (Neo) Khan <mahdeen.s.khan@yale.edu>
2021-04-18 20:25:00 -0400

    have both live and exercise feed up side by side

--- src/App.js
+++ src/App.js
@@ -1,45 +1,24 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Frames from "./Frames";
+import Exercises from './Exercises';
 import configData from "./config.json";
 
 function App() {
-  const [src, setSrc] = useState();
+  const [live, setLive] = useState();
+  const [exercise, setExercise] = useState();
   const [data, setData] = useState();
 
   useEffect(() => {
-    Frames(configData.devIP, setData, setSrc).start();
+    Frames(configData.prodIP, setData, setLive).start();
+    Exercises(setData, setExercise).start();
   }, []);
 
   return (
     <div className="App">
       <div>Live Feed</div>
-      <img src={src} alt="live_feed" />
-      {data && data.people
-        ? Object.keys(data.people).map((person, index) => {
-            // console.log(data.people[person]);
-            const keypoints = data.people[person].keypoints;
-            if (keypoints.RElbow && keypoints.LElbow)
-              return (
-                <div key={index}>
-                  <div>Person {person}</div>
-                  <div>X-Coordinate: {keypoints.RElbow[0].toFixed(2)}</div>
-                  <div>Y-Coordinate: {keypoints.RElbow[1].toFixed(2)}</div>
-                  <div>Z-Coordinate: {keypoints.RElbow[2].toFixed(2)}</div>
-                  <div>
-                    {keypoints.RElbow[1] < 0 && keypoints.LElbow[1] < 0
-                      ? "Both Arms Raised!"
-                      : keypoints.RElbow[1] < 0
-                      ? "Right Arm Raised!"
-                      : keypoints.LElbow[1] < 0
-                      ? "Left Arm Raised!"
-                      : "Nothing"}
-                  </div>
-                </div>
-              );
-            return null;
-          })
-        : null}
+      <img className="live" src={live} alt="live feed" />
+      <img className="exercise" src={exercise} alt="exercise feed" />
     </div>
   );
 }
--- src/Frames.js
+++ src/Frames.js
@@ -1,16 +1,14 @@
-function Frames(ip, setData, setSrc) {
+function Frames(ip, setData, setLive) {
   const url_frames = (ip) => `ws://${ip}:8888/frames`;
   const url_twod = (ip) => `ws://${ip}:8888/twod`;
-  const recordedFr = 'ws://127.0.0.1:4444/frames';
-  const recorded2D = 'ws://127.0.0.1:4444/twod';
 
   const framesObj = {
     socket_frames: null,
     socket_twod: null,
 
     start: () => {
-      framesObj.socket_frames = new WebSocket(recordedFr);
-      framesObj.socket_twod = new WebSocket(recorded2D);
+      framesObj.socket_frames = new WebSocket(url_frames(ip));
+      framesObj.socket_twod = new WebSocket(url_twod(ip));
       framesObj.socket_frames.onmessage = function (event) {
         framesObj.store(JSON.parse(event.data));
       };
@@ -20,7 +18,7 @@ function Frames(ip, setData, setSrc) {
     },
 
     show: (frame) => {
-      setSrc("data:image/pnjpegg;base64," + frame.src);
+      setLive("data:image/pnjpegg;base64," + frame.src);
     },
 
     store: (frame) => {
