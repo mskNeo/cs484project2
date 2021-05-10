b35c0d0c6b6391641e941c384190e14454c10c8f
Max Yuan <maxyuan6717@gmail.com>
2021-05-10 17:25:16 -0400

    noice

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ import moment from "moment";
 
 const exercises = ["arm_circles", "knee_drives", "jumping_jacks"];
 let twods = [];
+let exercise_frames = [];
 exercises.forEach((exercise) => {
   const twod = require(`./recordings_json/twod/${exercise}.json`);
   twods.push(twod);
+  const exercise_frame = require(`./recordings_json/frames/${exercise}.json`);
+  exercise_frames.push(exercise_frame);
 });
 
 function App() {
@@ -22,6 +25,7 @@ function App() {
   const [exeData, setExeData] = useState();
   const [start_time, setStartTime] = useState();
   const [frame_index, setFrameIndex] = useState(0);
+  const [score, setScore] = useState(0);
 
   useEffect(() => {
     Frames(configData.prodIP, setLiveData, setLive).start();
@@ -30,14 +34,26 @@ function App() {
   useEffect(() => {
     const interval = setInterval(() => {
       setFrameIndex(frame_index + 1);
-    }, 300);
+    }, 250);
     return () => clearInterval(interval);
   }, [frame_index]);
 
   useEffect(() => {
-    console.log(liveData);
-    console.log(select);
-    console.log(ready);
+    // console.log(liveData);
+    // console.log(select);
+    // console.log(ready);
+    const model_keys = Object.keys(
+      exercise_frames[exercise][frame_index % twods[exercise].length].people
+        ? exercise_frames[exercise][frame_index % twods[exercise].length].people
+        : {}
+    );
+    const model =
+      model_keys.length > 0
+        ? exercise_frames[exercise][
+            frame_index % exercise_frames[exercise].length
+          ].people[model_keys[0]].keypoints
+        : null;
+
     if (liveData && liveData.people) {
       let count = 0;
       for (let i = 0; i < Object.keys(liveData.people).length; i++) {
@@ -45,25 +61,34 @@ function App() {
           count++;
         }
       }
-      console.log(count);
+      // console.log(count);
       if (count === 0) {
         setSelect(false);
         setMessage("Come workout with us!");
       }
-      if (count > 1) {
+      if (!start && !ready && !select && count > 1) {
         setSelect(false);
         setMessage("Only one person, please. I'm scared of large crowds.");
-      }
-      else {
+      } else {
         Object.keys(liveData.people).forEach((person) => {
           const pos = liveData.people[person].avg_position;
           const keypoints = liveData.people[person].keypoints;
-          if (keypoints.RElbow && keypoints.RShoulder && keypoints.LElbow && keypoints.LShoulder && !start) {
+          if (
+            keypoints.RElbow &&
+            keypoints.RShoulder &&
+            keypoints.LElbow &&
+            keypoints.LShoulder &&
+            !start
+          ) {
             // if wrists are above people's eyes/heads, they're up
-            if (!select && !ready && !start) {
+            if (!select && !ready) {
               if (pos[2] > 3350) {
                 return;
-              } else if ((keypoints.RElbow[1] > keypoints.RShoulder[1] || keypoints.LElbow[1] > keypoints.LShoulder[1]) && !select) {
+              } else if (
+                (keypoints.RElbow[1] > keypoints.RShoulder[1] ||
+                  keypoints.LElbow[1] > keypoints.LShoulder[1]) &&
+                !select
+              ) {
                 setMessage(
                   "Raise your hands high like you're trying to touch the sky."
                 );
@@ -71,17 +96,27 @@ function App() {
                 setMessage("Ok, put your hands down.");
                 setSelect(true);
               }
-            } 
-            else if (select && !ready && !start) {
-              if (keypoints.RElbow[1] > keypoints.RShoulder[1] && keypoints.LElbow[1] > keypoints.LShoulder[1]) {                
+            } else if (select && !ready) {
+              if (
+                keypoints.RElbow[1] > keypoints.RShoulder[1] &&
+                keypoints.LElbow[1] > keypoints.LShoulder[1]
+              ) {
                 setReady(true);
-              } 
-            }
-            else if (select && ready && !start) {
-              if (keypoints.RElbow[1] > keypoints.RShoulder[1] && keypoints.LElbow[1] > keypoints.LShoulder[1]) {
-                setMessage("Pick an exercise. Raise your right hand for an easy one, left hand for a medium one, and both hands for a harder one.");                
+              }
+            } else if (select && ready) {
+              if (
+                keypoints.RElbow[1] > keypoints.RShoulder[1] &&
+                keypoints.LElbow[1] > keypoints.LShoulder[1]
+              ) {
+                setMessage(
+                  "Pick an exercise. Raise your right hand for an easy one, left hand for a medium one, and both hands for a harder one."
+                );
               } // both hands, hard exercise
-              else if (ready && keypoints.RElbow[1] < keypoints.RShoulder[1] && keypoints.LElbow[1] < keypoints.LShoulder[1]) {
+              else if (
+                ready &&
+                keypoints.RElbow[1] < keypoints.RShoulder[1] &&
+                keypoints.LElbow[1] < keypoints.LShoulder[1]
+              ) {
                 setMessage("Get ready! Start, now!");
                 setTimeout(() => {
                   setStart(true);
@@ -110,43 +145,76 @@ function App() {
                   setStartTime(Date.now());
                 }, 300);
               }
-            } 
+            }
+          }
+          if (start && model && pos[2] < 3350) {
+            const arr = ["RElbow", "RKnee", "LKnee", "LElbow"];
+            let temp = score;
+            arr.forEach((point) => {
+              temp +=
+                keypoints[point] &&
+                keypoints[point][1] &&
+                model[point] &&
+                model[point][1]
+                  ? Math.abs(keypoints[point][1] - model[point][1])
+                  : 0;
+            });
+            setScore(temp);
           }
         });
       }
     }
-  }, [liveData, message, start, select, ready]);
+  }, [liveData, message, start, select, ready, exercise, frame_index]);
 
   const timer = moment().diff(start_time, "seconds");
 
   if (timer === 30) {
     setStart(false);
-    setSelect(false);
-    setReady(false);
+
     setStartTime(null);
 
     setMessage("You're done! Go enjoy your day :)");
-
     setTimeout(() => {
       setMessage("Come workout with us!");
-    }, 5000);
+      setSelect(false);
+      setReady(false);
+    }, 10000);
   }
-  
 
   return (
     <div className="view">
-      {start 
-        ? 
-          <>
-            <img className="live" src={live} alt="live feed" />
-            {start_time ? <div id="timer">{30 - timer}</div> : null}
-            <img className="exercise" src={`data:image/pnjpegg;base64,${
-                twods[exercise][frame_index % twods[exercise].length].src
-              }`} alt="exercise feed" />
-          </>
-        : 
+      {start ? (
+        <>
+          <img className="live" src={live} alt="live feed" />
+          {start_time ? <div id="timer">{30 - timer}</div> : null}
+          {start_time && timer >= 10 && timer <= 12 ? (
+            <div id="encourage">Keep Moving</div>
+          ) : null}
+          {start_time && timer >= 20 && timer <= 22 ? (
+            <div id="encourage">Looks Good!</div>
+          ) : null}
+          {start_time && timer >= 26 && timer <= 28 ? (
+            <div id="encourage">Almost There :)</div>
+          ) : null}
+          <img
+            className="exercise"
+            src={`data:image/pnjpegg;base64,${
+              twods[exercise][frame_index % twods[exercise].length].src
+            }`}
+            alt="exercise feed"
+          />
+        </>
+      ) : (
+        <div>
           <h1 id="message">{message}</h1>
-      }
+          <br />
+          {message === "You're done! Go enjoy your day :)" && (
+            <h1 id="message">
+              Your Score: {Math.round((1000000 - score) / 10000)}/100
+            </h1>
+          )}
+        </div>
+      )}
     </div>
   );
 }
